1e3f2c4a4fda07be34ea6c5981ccd31985e98365
Larysa <seagull2610@gmail.com>
2024-10-07 17:16:11 +0200

    add task 2

--- js/task-3.js
+++ js/task-3.js
@@ -8,17 +8,6 @@ function filterArray(numbers, value) {
     return arr;
 }
 
-// function filterArray(numbers, value) {
-//  let arrayNew = [];
-
-//   for (let i = 0; i < numbers.length; i ++) {
-//       if (numbers[i] > value) {
-//         arrayNew.push(numbers[i]);
-//       }
-//     }
-//   return arrayNew;
-// }
-
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
